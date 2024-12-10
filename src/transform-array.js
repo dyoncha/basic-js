24ad0ff94cf7d6c8ef1e75f0d73c8e87281ed17a
dyoncha <dyoncha77@gmail.com>
2024-12-11 00:45:09 +0300

    feat: add transform fn

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (!arr.length) {
+    return arr;
+  }
+
+  let result = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-prev':
+        if (arr[i - 1] !== 'toDelete') {
+          result.pop();
+          result.push('toDelete');
+        }
+        break;
+      case '--double-next':
+          if(arr[i + 1]) {
+              result.push(arr[i + 1]);
+          }
+          break;
+      case '--discard-next':
+          result.push('toDelete');
+          i++;
+          break;
+      case '--double-prev':
+          if (result.length && result[result.length - 1] !== 'toDelete') {
+              result.push(result[result.length - 1]);
+          }
+          break;
+      default: 
+          result.push(arr[i]);
+          break;
+      }
+  }
+
+  result = result.filter(item => item !== 'toDelete');
+
+  return result;
 }
 
 module.exports = {
